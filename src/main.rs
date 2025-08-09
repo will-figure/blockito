bfbe28be67d784510d990495cbb28470301cf9d1
Will LaBranche <wlabranche@figure.com>
2025-08-09 16:37:57 -0700

    start parting things out

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use axum::{Extension, Json};
+mod http;
+mod model;
+
 use axum::{
     Router,
     http::{StatusCode, Uri},
@@ -8,6 +10,8 @@ use axum::{
 use serde_json::json;
 use std::collections::HashMap;
 use tokio::net::TcpListener;
+
+use crate::model::bother::Bother;
 
 const PORT: &str = "8123";
 
@@ -21,23 +25,19 @@ async fn health() -> impl IntoResponse {
     (StatusCode::OK, axum::Json(map))
 }
 
-struct Bother {
-    user_id: String,         // TODO: use uuid
-    conversation_id: String, // TODO: use uuid
-    messages: String,
-}
-
 // TODO: use extenstion for with db
-async fn bother_blockito() -> impl IntoResponse {
-    // TODO: chat history should be stored/addedadded
+async fn bother_blockito(axum::Json(bother): axum::Json<Bother>) -> impl IntoResponse {
+    // TODO: expand on system prompt and figure out what data to train with and what model to use
     let messages = vec![json!({
         "role": "system",
         "content": "your name is blockito and you are neat"
     })];
+    // get the conversation messages from the database
+    // TODO: chat history should be stored/addedadded
     // TODO push in all historical
     let request = json!({
         "model": "PLACEHOLDER",
-        "prompt": "what's your favorite cheese?",
+        "prompt": bother.message,
         "messages": messages,
     });
 
@@ -51,9 +51,8 @@ async fn bother_blockito() -> impl IntoResponse {
         .text()
         .await
         .unwrap(); // TODO: no unwrap
-    println!("response body: {:?}", body);
     // TODO: parse the response and return something useful
-    (StatusCode::OK, body)
+    (StatusCode::OK, axum::Json(body))
 }
 
 #[tokio::main]
@@ -61,10 +60,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!!!!!!");
 
     let llama_router = Router::new()
-        // TODO: shouldn't be a get
         .route("/", get(health))
         .route("/health", get(health))
-        .route("/bother", get(bother_blockito))
+        .route("/bother", post(bother_blockito))
         .fallback(fallback);
 
     let listener = TcpListener::bind(format!("127.0.0.1:{PORT}")).await?;
